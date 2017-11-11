0484c570ee2f0ad9d179e7055fd11570b23f1fed
SangKil Park <ht1911@gmail.com>
2017-11-11 21:20:28 +0900

    지수 정렬 로직 수정

--- src/sort/RadixSort.java
+++ src/sort/RadixSort.java
@@ -1,34 +1,154 @@
 package sort;
 
-import java.util.Arrays;
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * on 31/12/2016.
+ */
+
+class Bucket {
+    int data;
+    Bucket node;
+
+    public Boolean isEmpty() {
+        if (this.node == null) {
+            return true;
+        }
+        return false;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Bucket getNode() {
+        return node;
+    }
+
+    public void setNode(Bucket node) {
+        this.node = node;
+    }
+
+    Bucket(int data) {
+        this.data = data;
+    }
+
+    Bucket(Bucket node) {
+        this.node = node;
+    }
+}
 
 public class RadixSort {
 
-    public static void main(String... args) {
-        int[] arr = new int[5];
-        arr[0] = 52;
-        arr[1] = 31;
-        arr[2] = 24;
-        arr[3] = 45;
-        arr[4] = 13;
+    Bucket[] bucketArr = new Bucket[10];
+    Bucket[] data;
 
-        sort(arr);
+    void inputNumber() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
 
-        Arrays.stream(arr).forEach(System.out::println);
-    }
-    public static int[] sort(int[] arr) {
-        int[] newArr = new int[100];
-        int[] result = new int[arr.length];
-        for (int item : arr) {
-            newArr[item] = item;
+        System.out.println("숫자 갯수를 입력하세요:");
+        int n = Integer.parseInt(st.nextToken());//숫자 갯수 입력
+//        int n = 4;
+        data = new Bucket[n];//숫자 저장소
+//        data[0] = new Bucket(231);
+//        data[1] = new Bucket(152);
+//        data[2] = new Bucket(675);
+//        data[3] = new Bucket(442);
+
+        System.out.println("숫자를 입력하세요:");
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine().trim());
+            data[i] = new Bucket(Integer.parseInt(st.nextToken()));
         }
+
+    }
+
+    void sort() throws IOException {
+        inputNumber();
+        // display();
+
+
+        Boolean keepGoing = true;
+        int cnt = 1;
+        int bucketNum = 0;
         int index = 0;
-        for (int item : newArr) {
-            if (item != 0) {
-                result[index++] = item;
+        while (keepGoing) {
+            keepGoing = false;
+
+            for (int i = 0; i < data.length; i++) {
+                //Bucket에 Data 넣기
+                int num = data[i].getData();
+                for (int k = 0; k < cnt; k++) {
+                    //자릿수
+                    bucketNum = num % 10;
+                    num /= 10;
+                }
+                if (bucketNum != 0) {
+                    keepGoing = true;
+                }
+                //Number number = new Number(data[i].getNum());
+                if (bucketArr[bucketNum] == null) {
+                    bucketArr[bucketNum] = data[i];
+                } else {
+
+                    Bucket temp = data[i];
+                    Bucket head = bucketArr[bucketNum];
+                    while (head.node != null) {
+                        head = head.node;
+                    }
+                    head.node = temp;
+                }
             }
+            //Bucket에 순서대로 빼기
+            index = 0;
+            for (int i = 0; i < bucketArr.length; i++) {
+                if (bucketArr[i] != null) {
+                    Bucket temp = bucketArr[i];
+                    do {
+                        data[index] = temp;
+                        temp = temp.node;
+                        data[index++].node = null;//data[]에 들어가면 링크 삭제
+                    } while (temp != null);
+                }
+            }
+            bucketInit();
+            cnt++;
         }
-        return result;
+        display();
+
     }
 
+    void bucketInit() {
+        //bucketArr 초기화
+        for (int i = 0; i < bucketArr.length; i++) {
+            bucketArr[i] = null;
+        }
+
+    }
+
+    void display() throws IOException {
+
+//        for (int i = 0; i < data.length; i++) {
+//            System.out.print(data[i].getData() + " ");
+//        }
+//        System.out.println();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < data.length; i++) {
+            bw.write(String.valueOf(data[i].getData()) + "\n");
+        }
+        bw.close();
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        new RadixSort().sort();
+    }
 }
